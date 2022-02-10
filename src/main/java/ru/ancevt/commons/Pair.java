f2c81f20ed9a66334baef0982d3427b2361304e9
Ancevt <i@ancevt.ru>
2022-02-10 11:42:26 +0300

    Initial commit

---
+++ src/main/java/ru/ancevt/commons/Pair.java
@@ -0,0 +1,48 @@
+/*
+ *   Ancevt Commons Library
+ *   Copyright (C) 2022 Ancevt (i@ancevt.ru)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package ru.ancevt.commons;
+
+public class Pair<L, R> {
+    private final L left;
+    private final R right;
+
+    private Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    public static <L, R> Pair<L, R> of(L left, R right) {
+        return new Pair<>(left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
---
+++ src/main/java/ru/ancevt/commons/concurrent/Lock.java
@@ -0,0 +1,59 @@
+/*
+ *   Ancevt Commons Library
+ *   Copyright (C) 2022 Ancevt (i@ancevt.ru)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package ru.ancevt.commons.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Lock {
+
+    private final CountDownLatch countDownLatch;
+    private State state;
+
+    public Lock() {
+        countDownLatch = new CountDownLatch(1);
+        state = State.BEFORE;
+    }
+
+    public void lock() {
+        try {
+            state = State.LOCKED;
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public void unlock() {
+        state = State.UNLOCKED;
+        countDownLatch.countDown();
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public boolean isLocked() {
+        return state == State.LOCKED;
+    }
+
+    public enum State {
+        BEFORE,
+        LOCKED,
+        UNLOCKED
+    }
+}
