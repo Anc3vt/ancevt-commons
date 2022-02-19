40e7c63f44988dd541f30ab29b89500c7f76192b
Ancevt <i@ancevt.ru>
2022-02-19 21:05:52 +0300

    PatternMatcher added

---
+++ src/main/java/ru/ancevt/commons/regex/PatternMatcher.java
@@ -0,0 +1,12 @@
+package ru.ancevt.commons.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcher {
+    public static boolean check(String text, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.matches();
+    }
+}
