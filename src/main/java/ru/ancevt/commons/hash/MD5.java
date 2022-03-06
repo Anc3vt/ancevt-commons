2241b5fa6ad181bdd13ac61a2b30d95f5f11a6bb
Ancevt <i@ancevt.ru>
2022-03-06 17:05:46 +0300

    MD5: add hashFile method

--- src/main/java/ru/ancevt/commons/hash/MD5.java
+++ src/main/java/ru/ancevt/commons/hash/MD5.java
@@ -1,6 +1,9 @@
 package ru.ancevt.commons.hash;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -17,8 +20,16 @@ public class MD5 {
         return bytesToHex(hash(string.getBytes(StandardCharsets.UTF_8)));
     }
 
+    public static String hashFile(String path) {
+        try {
+            return bytesToHex(hash(Files.readAllBytes(Path.of(path))));
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(MD5.hash("hello"));
+        System.out.println(MD5.hashFile("README.md"));
     }
 
     private static String bytesToHex(byte[] bytes) {
