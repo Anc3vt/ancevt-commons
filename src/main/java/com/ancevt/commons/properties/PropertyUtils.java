67fe5cd9d6a7e3cf6a0269789513e76de6f58627
Ancevt <i@ancevt.ru>
2022-03-29 16:49:00 +0300

    2022-03-29_16-49-00

---
+++ src/main/java/com/ancevt/commons/properties/PropertyUtils.java
@@ -0,0 +1,20 @@
+package com.ancevt.commons.properties;
+
+import java.util.Properties;
+
+public class PropertyUtils {
+
+    public static Properties argsToProperties(String[] args, Properties properties) {
+        for (String arg : args) {
+            if (arg.startsWith("-P")) {
+                arg = arg.substring(2);
+                String[] split = arg.split("=");
+                String key = split[0];
+                String value = split[1];
+                properties.setProperty(key, value);
+            }
+        }
+
+        return properties;
+    }
+}
