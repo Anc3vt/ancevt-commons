2757b2eb6c0a3e011c2751bb81c696a5efea6de1
Ancevt <i@ancevt.ru>
2022-05-21 05:39:29 +0300

    Add OsDetector; Add license headers

---
+++ src/main/java/com/ancevt/commons/platformdepend/OsDetector.java
@@ -0,0 +1,59 @@
+/*
+ *   Ancevt Commons Library
+ *   Copyright (C) 2022 Ancevt (me@ancevt.com)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ancevt.commons.platformdepend;
+
+public class OsDetector {
+
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isWindows() {
+        return OS.contains("win");
+    }
+
+    public static boolean isMac() {
+        return OS.contains("mac");
+    }
+
+    public static boolean isUnix() {
+        return OS.contains("nix")
+                || OS.contains("nux")
+                || OS.indexOf("aix") > 0;
+    }
+
+    public static boolean isSolaris() {
+        return OS.contains("sunos");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("os.name: " + OS);
+
+        if (isWindows()) {
+            System.out.println("This is Windows");
+        } else if (isMac()) {
+            System.out.println("This is Mac");
+        } else if (isUnix()) {
+            System.out.println("This is Unix or Linux");
+        } else if (isSolaris()) {
+            System.out.println("This is Solaris");
+        } else {
+            System.out.println("Your OS is not support!!");
+        }
+    }
+
+}
+
