7eafad444dcf0e02cb246df6fa64841ec2ca25d5
Ancevt <i@ancevt.ru>
2022-05-23 00:30:24 +0300

    Add MainClassNameExctractor

---
+++ src/main/java/com/ancevt/commons/concurrent/MainClassNameExtractor.java
@@ -0,0 +1,30 @@
+package com.ancevt.commons.concurrent;
+
+import java.util.Map;
+
+public class MainClassNameExtractor {
+
+    public static String get() {
+        Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+        for (Map.Entry<Thread, StackTraceElement[]> entry : map.entrySet()) {
+            Thread thread = entry.getKey();
+            if (thread.getId() == 1) {
+                StackTraceElement[] stackTraceElements = entry.getValue();
+                for (int i = stackTraceElements.length - 1; i >= 0; i++) {
+                    StackTraceElement stackTraceElement = stackTraceElements[i];
+                    if (stackTraceElement.getMethodName().equals("main")) {
+                        return stackTraceElement.getClassName();
+                    }
+                }
+            }
+        }
+        throw new MainClassNameExtractorException("Unable to extract application main class name");
+    }
+
+    public static class MainClassNameExtractorException extends RuntimeException {
+
+        public MainClassNameExtractorException(String message) {
+            super(message);
+        }
+    }
+}
